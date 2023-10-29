f5e7bc9cb538541ca3638bab5fa7f57f50b63403
joaco659 <joaco659782@gmail.com>
2023-10-29 13:22:32 -0300

    Cartas 3D agregadas.

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,70 @@ const TECHNOLOGIES = [
   "React.js",
 ];
 
+class Project {
+  constructor(iconURL, title, description, githubLink, previewLink) {
+    this.iconURL = iconURL;
+    this.title = title;
+    this.description = description;
+    this.githubLink = githubLink;
+    this.previewLink = previewLink;
+  }
+
+  build() {
+    // const acceptedLinks = {
+    //   github: <i className="fa-brands fa-github"></i>,
+    // };
+    /* {Object.entries(acceptedLinks).map(icon => {
+            Object.entries(this.links).map(link => {
+              if (icon[0] == link[0]) {
+                return icon[1]
+              }
+            })
+          })} */
+
+    return (
+      <div className="project-card">
+        <div
+          className="icon"
+          style={{ backgroundImage: `url(${this.iconURL})` }}
+        ></div>
+        <h4 className="title">{this.title}</h4>
+        <article className="description">
+          <p>{this.description}</p>
+        </article>
+        <ul className="links">
+          <li>
+            <a href={this.githubLink} target="_blank" rel="noreferrer">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+const PROJECTS = [
+  new Project(
+    "./layered-peaks-1-dark.svg",
+    "Lorem ipsum dolor sit amet",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi tempore quos architecto pariatur non, voluptas corporis nostrum incidunt iste dignissimos eveniet aliquam, deleniti, ratione doloribus at ipsam cumque voluptatum quo.",
+    "#"
+  ),
+  new Project(
+    "./layered-peaks-1-dark.svg",
+    "Lorem ipsum dolor sit amet",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi tempore quos architecto pariatur non, voluptas corporis nostrum incidunt iste dignissimos eveniet aliquam, deleniti, ratione doloribus at ipsam cumque voluptatum quo.",
+    "#"
+  ),
+  new Project(
+    "./layered-peaks-1-dark.svg",
+    "Lorem ipsum dolor sit amet",
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi tempore quos architecto pariatur non, voluptas corporis nostrum incidunt iste dignissimos eveniet aliquam, deleniti, ratione doloribus at ipsam cumque voluptatum quo.",
+    "#"
+  ),
+];
+
 function App() {
   const [theme, setTheme] = useState("dark");
 
@@ -76,27 +140,9 @@ function App() {
             De todos los proyectos que he hecho, destaco solo los siguientes.
           </p>
 
-          <AnimatedCard>
-            <div className="project-card">
-              <div className="icon"></div>
-              <h4 className="title">Prueba de tarjeta 3D</h4>
-              <article className="description">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                  doloribus inventore, eum perspiciatis dicta dolor itaque amet
-                  laudantium asperiores consequuntur a eligendi incidunt
-                  nesciunt tenetur omnis, non recusandae debitis suscipit?
-                </p>
-              </article>
-              <ul className="links">
-                <li>
-                  <a href="#" target="_blank">
-                    <i className="fa-brands fa-github"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </AnimatedCard>
+          {PROJECTS.map((project, i) => (
+            <AnimatedCard key={i}>{project.build()}</AnimatedCard>
+          ))}
         </div>
 
         <footer className="credits">
